1ec3a8c6f2bb6a58fda4c3f2a08a213e9a4c7486
abusaiyedjoy <abusaiyedjoy1@gmail.com>
2024-07-09 14:29:19 +0600

    Modify cart $ create button componets

--- src/Pages/Home/TopSelling/TopSelling.jsx
+++ src/Pages/Home/TopSelling/TopSelling.jsx
@@ -1,3 +1,5 @@
+import FirstButton from "../../../Components/Button/FirstButton";
+import SecondButton from "../../../Components/Button/SecondButton";
 import Title from "../../../Components/Title/Title";
 
 const TopSelling = () => {
@@ -10,9 +12,9 @@ const TopSelling = () => {
                     <div className="card-body items-center text-center pt-32">
                         <h2 className="card-title">Mango TbTruckDelivery</h2>
                         <p>$20.00</p>
-                        <div className="card-actions justify-end">
-                            <button className="btn btn-primary">Add Cart</button>
-                            <button className="btn btn-secondary">Add Wishlist</button>
+                        <div className="justify-end flex items-end  ">
+                            <SecondButton name={"Add to Cart"}></SecondButton>
+                            <FirstButton name={"Add to Wishlist"}></FirstButton>
                         </div>
                     </div>
                 </div>
