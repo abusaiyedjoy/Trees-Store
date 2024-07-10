658d5d9a8e4a24c9a76db998854f19e69100128f
abusaiyedjoy <abusaiyedjoy1@gmail.com>
2024-07-10 23:16:22 +0600

    Update cart section

--- src/Pages/Home/TopSelling/TopSelling.jsx
+++ src/Pages/Home/TopSelling/TopSelling.jsx
@@ -6,7 +6,40 @@ const TopSelling = () => {
     return (
         <>
         <Title title={"Our Top Selling Products"}></Title>
-            <section className=" mt-44 mb-12 container mx-auto">
+            <section className=" mt-44 mb-12 container mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+                <div className="card border relative border-primary bg-secondary w-96">
+                    <img className="absolute -top-7 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-56" src="https://i.ibb.co/NY4gJqg/Plant-2-removebg-preview.png" alt="" />
+                    <div className="card-body items-center text-center pt-32">
+                        <h2 className="card-title">Mango TbTruckDelivery</h2>
+                        <p>$20.00</p>
+                        <div className="justify-end flex items-end  ">
+                            <SecondButton name={"Add to Cart"}></SecondButton>
+                            <FirstButton name={"Add to Wishlist"}></FirstButton>
+                        </div>
+                    </div>
+                </div>
+                <div className="card border relative border-primary bg-secondary w-96">
+                    <img className="absolute -top-7 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-56" src="https://i.ibb.co/NY4gJqg/Plant-2-removebg-preview.png" alt="" />
+                    <div className="card-body items-center text-center pt-32">
+                        <h2 className="card-title">Mango TbTruckDelivery</h2>
+                        <p>$20.00</p>
+                        <div className="justify-end flex items-end  ">
+                            <SecondButton name={"Add to Cart"}></SecondButton>
+                            <FirstButton name={"Add to Wishlist"}></FirstButton>
+                        </div>
+                    </div>
+                </div>
+                <div className="card border relative border-primary bg-secondary w-96">
+                    <img className="absolute -top-7 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-56" src="https://i.ibb.co/NY4gJqg/Plant-2-removebg-preview.png" alt="" />
+                    <div className="card-body items-center text-center pt-32">
+                        <h2 className="card-title">Mango TbTruckDelivery</h2>
+                        <p>$20.00</p>
+                        <div className="justify-end flex items-end  ">
+                            <SecondButton name={"Add to Cart"}></SecondButton>
+                            <FirstButton name={"Add to Wishlist"}></FirstButton>
+                        </div>
+                    </div>
+                </div>
                 <div className="card border relative border-primary bg-secondary w-96">
                     <img className="absolute -top-7 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-56" src="https://i.ibb.co/NY4gJqg/Plant-2-removebg-preview.png" alt="" />
                     <div className="card-body items-center text-center pt-32">
